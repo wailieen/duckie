7c257f6861790b3542084d3083863682065cb9bb
wailieen <79746913+wailieen@users.noreply.github.com>
2021-12-21 21:24:18 +0300

    duckie 2

---
+++ duckie2.py
@@ -0,0 +1,58 @@
+from gym_duckietown.tasks.task_solution import TaskSolution
+import numpy as np
+import cv2 as cv
+
+class DontCrushDuckieTaskSolution(TaskSolution):
+	def __init__(self, generated_task):
+		super().__init__(generated_task)
+
+	def solve(self):
+		env = self.generated_task['env']
+		img, _, _, _ = env.step([0,0])
+		
+		condition = True
+		
+		while condition:
+			img, reward, done, info = env.step([1, 0])
+			
+			img_rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+			lower_range = np.array([0, 190, 250])
+			upper_range = np.array([1, 220, 255])
+
+			mask = cv.inRange(img_rgb, lower_range, upper_range)
+			contours, hierarchy = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+			x, y, w, h = cv.boundingRect(contours[0])
+			if (h > 100):
+				condition = False
+
+				#changing lane
+				img, reward, done, info = env.step([0, 45])
+				env.render()
+				for i in range (0,10):
+				    img, reward, done, info = env.step([1, 0])
+				env.render()
+				img, reward, done, info = env.step([0, -45])
+				env.render()
+				for i in range (0,10):
+				    img, reward, done, info = env.step([1, 0])
+				env.render()
+
+				#returning back
+				img, reward, done, info = env.step([0, -45])
+				env.render()
+				for i in range (0,10):
+				    img, reward, done, info = env.step([1, 0])
+				env.render()
+				img, reward, done, info = env.step([0, 45])
+				env.render()
+				for i in range (0,10):
+				    img, reward, done, info = env.step([1, 0])
+				env.render()
+				
+				#stop
+				img, reward, done, info = env.step([0, 0])
+				env.render()
+			else:
+				condition = True
+			env.render()
